8d9b240d7be9734de3e644bb752df578ff311f2b
Romel Benavides <rome_benavides@dusty.tamiu.edu>
2018-09-05 23:55:14 -0500

    Initial upload

---
+++ JS Exercise/script.js
@@ -0,0 +1,16 @@
+/*
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+fullName = firstName + " " + lastName;
+
+console.log("Your full name is, " + fullName + "and your age is " + age);
+*/
+
+var age = prompt("Please enter your age here: ");
+
+var alive = 365 * age;
+
+alert("You have lived " + alive + " days.");
+console.log("You have lived " + alive + " days.");
