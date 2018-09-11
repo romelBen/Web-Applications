14a5a7f9636189583e7305617949be10dcf5f220
Romel Benavides <rome_benavides@dusty.tamiu.edu>
2018-09-10 19:26:39 -0500

    Update exercises

--- JS Exercise/script.js
+++ JS Exercise/script.js
@@ -6,7 +6,7 @@ var age = prompt("What is your age?");
 fullName = firstName + " " + lastName;
 
 console.log("Your full name is, " + fullName + "and your age is " + age);
-*/
+
 
 var age = prompt("Please enter your age here: ");
 
@@ -14,3 +14,21 @@ var alive = 365 * age;
 
 alert("You have lived " + alive + " days.");
 console.log("You have lived " + alive + " days.");
+*/
+
+/* Stacking Arrays */
+var friends = ["Aneki", "Quell", "Clarity", "Sleepy", "Roghar", "DM Crimson"];
+
+var team1 = new Array();
+var team2 = new Array();
+
+for (var i = 0; i < friends.length; i++) {
+    if (i % 2 == 0) {
+        // if the index is even, add the person to team 1
+        team1.push(friends[i]);
+    } else {
+        // otherwise add them to team 2
+        team2.push(friends[i]);
+    }
+}
+
