514c6163de0d744df5ce80ac5c41e882853df99c
Romel Benavides <rome_benavides@dusty.tamiu.edu>
2019-10-23 19:27:39 -0500

    Web application push

---
+++ Javascript Projects/Code.js
@@ -0,0 +1,20 @@
+/* Lecture: Hoisting */
+
+function calculateAge(birthYear) {
+    return 2018 - birthYear;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJohn, ageMike, ageJane);
+
+
+function yearsUntilRetirement(year, firstName) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    console.log(firstName + ' retires in ' + retirement + ' years.');
+}
+
+yearsUntilRetirement(1990, 'John');
+
